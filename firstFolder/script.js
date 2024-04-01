2d95dbf2337025f37f0ba9f46ccaaefb309f82a0
jais1441 <jaiswalvikas7276@gmail.com>
2024-04-01 22:20:48 +0530

    Installing packages via npm and useage

--- firstFolder/script.js
+++ firstFolder/script.js
@@ -1,2 +1,13 @@
-var a = 10;
-module.exports =a;
+// var a = 10;
+// module.exports =a;
+
+var figlet = require("figlet");
+
+figlet("Node", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
