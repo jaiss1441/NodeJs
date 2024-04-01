b745a6f0a8a64e50d2cedd1f45009b2b6f9d7c1d
jais1441 <jaiswalvikas7276@gmail.com>
2024-04-01 16:59:00 +0530

    Ecplanation of Nodw JS

--- firstFolder/script.js
+++ firstFolder/script.js
@@ -1,3 +1,10 @@
 // Node JS is large code of c++ which getted from crome v8 engine and wrapped into JS 
 //which is allow us to write JS and accept JS and 
 //on the basis of C++ it help us to create server environment, till node is running  
+
+// What is NodeJS in simple terms?
+
+//  Node. js (Node) is an Open Source, cross-platform runtime environment for executing JavaScript code.
+//  Node is used extensively for server-side programming, 
+//  making it possible for developers to use JavaScript for 
+//  client-side and server-side code without needing to learn an additional language.
