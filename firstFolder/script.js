d892cce3001ae915117caa6d07c8e1a01ea91b0a
jais1441 <jaiswalvikas7276@gmail.com>
2024-04-01 16:47:04 +0530

    Ecplanation of Nodw JS

---
+++ firstFolder/script.js
@@ -0,0 +1,3 @@
+// Node JS is large code of c++ which getted from crome v8 engine and wrapped into JS 
+//which is allow us to write JS and accept JS and 
+//on the basis of C++ it help us to create server environment, till node is running  
